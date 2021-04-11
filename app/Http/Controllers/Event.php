591d17fda18f073c7dbe6430317eec4b55f542f9
hiruni25 <hirudesilva25@gmail.com>
2021-04-11 15:16:16 +0530

    "Implemented EventController"

--- app/Http/Controllers/Event.php
+++ app/Http/Controllers/Event.php
@@ -3,8 +3,113 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
+use App\User;
+use App\EventUser;
+use Illuminate\support\Facades\Mail;
+use App\mail\notifyUser;
+use GuzzleHttp\Psr7\Message;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
-class Event extends Controller
+
+class EventController extends Controller
 {
-    //
+    private $userType;
+
+    //send data to database
+
+    public function store(Request $request)
+    {
+        $sdate=$request->get('sdate');
+        $event=Event::create($request->all());
+    }
+
+    public function dateCheck(){
+        $isExist= Event::select("*")
+                ->where("sdate",$sdate)
+                ->doesntExist();
+
+        if($isExist){
+            return response('isexist');
+        }
+        else{
+            return response('else');
+        }
+        return response()->json(['message'=>
+        "Event created successfully"]);
+    }
+
+    //get data by id
+    public function getEventById($id)
+    {
+       $event=event::find($id);
+       if(is_null($event)){
+        return response()->json(['message'=>'Event Not Found'],404); 
+    }
+        return response()->json($event::find($id),200);
+    }
+    
+    //save updated data to database
+    public function updateEvent(Request $request,$id)
+    {   
+        $event=event::find($id);
+        if(is_null($event)){
+            return response()->json(['message'=>'Event Not Found'],404); 
+        }
+        $event->update($request->all());
+        return response()->json(['message'=>
+        "Event updated successfully"]);
+        //return response ($event,200);
+        
+    }
+
+    //delete event
+    public function deleteEvent(Request $request,$id)
+    {
+        $event = Event::find($id);
+        if(is_null($event)){
+            return response()->json(['message'=>'Event Not Found'],404);
+        }
+        $event->delete();
+        return response()->json(['message'=>
+        "Event deleted successfully"]);
+        //return response()->json(null,204);
+        //echo "successfully deleted.";
+        //return redirect()->to('/viewEvent');
+
+        //return back;
+    }
+
+    public function getAllEvents()
+    {
+        $event = event::get()->toJson(JSON_PRETTY_PRINT);
+        return response($event, 200);
+    }
+
+    /*public function getUserEmail(Request $req){
+        $userType=$req.u_type;
+        dd($userType);
+    }*/
+
+    public function mail()
+    {
+        
+        Mail::to('dilki@gmail.com')->send(new notifyUser());
+        return response()->json(['message'=>
+        "Email sent successfully"]);
+    }
+    
+    public function getPollEvents(){
+        $poll=DB::table('events')
+                ->where('isPoll','=','1')
+                ->get()->toJson(JSON_PRETTY_PRINT);
+        return response($poll,200);
+    }    
+
+    public function getAllPoll()
+    {
+        $poll = EventUser::get()->toJson(JSON_PRETTY_PRINT);
+        return response($poll, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    //get all events from event table
+    Route::get('events', 'EventController@getAllEvents');
+
+    //get event from event table using event id
+    Route::get('getEventById/{id}', 'EventController@getEventById');
+
+    //save events t the event table
+    Route::post('saveEvent', 'EventController@store');
+
+    //update events
+    Route::put('updateEvent/{id}', 'EventController@updateEvent');
+
+    //delete events
+    Route::delete('deleteEvent/{id}','EventController@deleteEvent');
+
+    //Email Route
+    Route::get('sendMail', [EventController::class, 'mail'])->name('email');
+
+    //get usertype and then derive email
+    Route::post('getUserEmail','EventController@getUserEmail');
+
+    Route::post('savePoll', 'EventController@createPoll');
+
+    //get all events that have created poll from event table
+    Route::get('getPollEvents','EventController@getPollEvents');
+
